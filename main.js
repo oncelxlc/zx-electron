706a7630b5020846606011d3dd932264c1a75770
ZheXian <84336243+oncelxlc@users.noreply.github.com>
2025-01-07 17:11:32 +0800

    feat(development): 添加本地开发服务器配置并优化应用启动流程
    - 在 angular.json 中添加 development 配置的 host 和 port
    -重构 main.js，实现本地开发时自动加载开发服务器 URL
    - 更新 package.json，添加新的开发脚本和依赖
    - 新增 preload.js，用于在主进程中暴露版本信息

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
-const {app, BrowserWindow} = require('electron');
-const url = require('url');
-const path = require('path');
+const {app, BrowserWindow} = require("electron");
+const {join} = require("node:path");
 
-function onReady () {
-  win = new BrowserWindow({width: 900, height: 6700})
-  win.loadURL(url.format({
-    pathname: path.join(
-      __dirname,
-      'dist/zx-electron/browser/index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+// 定义创建窗口的函数
+function createWindow() {
+  // 创建一个新的 BrowserWindow 实例
+  const win = new BrowserWindow({
+    // 设置窗口的 web 偏好设置
+    webPreferences: {
+      // 加载一个预加载脚本
+      preload: join(__dirname, 'preload.js'),
+      // 不允许在渲染进程中使用 Node.js 模块
+      nodeIntegration: false,
+      // 隔离上下文
+      contextIsolation: true,
+      // 禁用远程模块
+      enableRemoteModule: false
+    },
+    // 设置窗口的宽度和高度
+    width: 1280,
+    height: 720,
+  });
+
+  // 如果应用没有被打包
+  if (!app.isPackaged) {
+    // 加载本地开发服务器的 URL
+    win.loadURL("http://localhost:9701");
+  }
+  // 如果应用已经被打包
+  else {
+    // 加载打包后的文件
+    win.loadFile("dist/zx-electron/browser/index.html");
+  }
 }
 
-app.on('ready', onReady);
+// 当 Electron 应用准备就绪时调用 createWindow 函数
+app.whenReady().then(() => {
+  createWindow();
+});
+
+// 当所有窗口都被关闭时，退出应用（除了 macOS）
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
